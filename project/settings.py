a89a69bedbed720da21caffc5d22fe1404b142fc
Moni252003 <monimoni252003@gmail.com>
2025-06-14 10:55:13 +0530

    allowed host

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)gchvx@)#%g#w6r41d&b@p^#p%v8jmdx9si98*5dr=ic%mn#*d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weather-apps-9yfe.onrender.com']
 
 
 # Application definition
